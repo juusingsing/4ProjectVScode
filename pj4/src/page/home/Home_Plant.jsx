21752708cb734b3e086e81e256b23b6368326dce
Heachi-si <icingsugar001@naver.com>
2025-06-02 14:46:22 +0900

    [김명리]

--- pj4/src/page/home/Home_Plant.jsx
+++ pj4/src/page/home/Home_Plant.jsx
@@ -3,13 +3,15 @@ import { Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 import Test from '../../image/Plant_Test_Main.png';
 import '../../css/home.css';
+import Footprint from '../../icon/Footprint_Icon.png'
+import Planticon from '../../icon/Plant_Icon.png'
 
 
 const Home_Plant= () => {
-  const [activeTab, setActiveTab] = useState(0);
+  const [activeTab, setActiveTab] = useState(1);
   const tabs = [
-    { name: "동물", link: "/Home_Pet.do" },
-    { name: "식물", link: "/Home_Plant.do" }
+{ name: "동물", link: "/Home_Pet.do",  icon: <img src={Footprint} alt="동물 아이콘" style={{ verticalAlign: 'middle', marginLeft: '4px', height: '15px' }} />},
+    { name: "식물", link: "/Home_Plant.do",  icon: <img src={Planticon} alt="식물 아이콘" style={{ verticalAlign: 'middle', marginLeft: '4px', height: '15px' }} />}
   ];
 
   return (
@@ -22,10 +24,16 @@ const Home_Plant= () => {
         {tabs.map((tab, index) => (
           <Link key={index} to={tab.link} onClick={() => setActiveTab(index)}>
             <button className={`Tab_Buttons ${activeTab === index ? "active" : ""}`}>
-              {tab.name}
+              <div className="Under_Line">{tab.name}{tab.icon}</div>
             </button>
           </Link>
         ))}
+      </div>
+      <div className="Home_Selectbox">
+      <select name="정렬">
+        <option value="name">이름 순</option>
+        <option value="date">등록일 순</option>
+      </select>
       </div>
     </Box>
   );
